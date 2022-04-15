27fe8891d6ed33488940fa44296849ddbdce89b7
Eduardo-Moller <93298848+Eduardo-Moller@users.noreply.github.com>
2022-04-15 17:03:16 -0300

    Add files via upload

---
+++ api/js/script.js
@@ -0,0 +1,17 @@
+
+function fun(url, callback) {
+    let xml = new XMLHttpRequest();
+    xml.onreadystatechange = function() {
+      if (xml.readyState == 4 && xml.status == 200) {
+        let x = JSON.parse(xml.responseText);
+        callback(x);
+      }
+    }
+    xml.open("GET", url, true);
+    xml.send();
+  }
+  fun('https://api.thedogapi.com/v1/images/search?size=full', function(x) {
+    var html = '<img src="' + x[0]["url"] + '">';
+    document.getElementById("conteudo").innerHTML = html;
+  });
+  
